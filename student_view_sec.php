6ad71eb2fd62457246cdf849c3e40a0c63999367
williamQyq <qyqfiles@hotmail.com>
2019-03-24 18:07:16 -0400

    fix mtor mtee count

--- student_view_sec.php
+++ student_view_sec.php
@@ -5,7 +5,6 @@
     $con = mysqli_connect($host, $username) or die('Unable To connect');
     $mydb = mysqli_select_db($con, $database) or die ('could not select database');
     $query_view_sec = "SELECT * FROM courses NATURAL JOIN sections_belong NATURAL JOIN timeSlot";
-    
 ?>
 <html>
 <head>
@@ -46,6 +45,29 @@
         $result = mysqli_query($con,$query_view_sec);
         if($result != NULL) {
             while($row = mysqli_fetch_array($result)) {
+                //count enrolled mtor in section
+                $query_count_mtor = "SELECT COUNT(mtor_id) FROM teach WHERE teach.cid = '$row[cid]'
+                AND teach.title = '$row[title]' AND teach.sec_id = '$row[sec_id]'";
+                //count enrolled mtee in section
+                $query_count_mtee = "SELECT COUNT(mtee_id) FROM enroll WHERE enroll.cid = '$row[cid]'
+                AND enroll.title = '$row[title]' AND enroll.sec_id = '$row[sec_id]'";
+                
+                if($result_cnt_mtor = mysqli_query($con,$query_count_mtor)){
+                    $mtor_cnt = mysqli_fetch_array($result_cnt_mtor);
+                }else{
+                    $mtor_cnt[0] = '0';
+                }
+                //free $result_cnt_mtor
+                mysqli_free_result($result_cnt_mtor);
+                
+                if($result_cnt_mtee = mysqli_query($con,$query_count_mtee)){
+                    $mtee_cnt = mysqli_fetch_array($result_cnt_mtee);
+                }else{
+                    $mtee_cnt[0] = '0';
+                }
+                //free result
+                mysqli_free_result($result_cnt_mtee);
+                
                 echo '<tr> 
                         <td>'.$row["title"].'</td>
                         <td>'.$row["name"].'</td>
@@ -55,8 +77,8 @@
                         <td>'.$row["capacity"].'</td>
                         <td>'.$row["mtors_req"].'</td>
                         <td>'.$row["mtees_req"].'</td> 
-                        <td>NA</td>
-                        <td>NA</td>
+                        <td>'.$mtor_cnt[0].'</td>
+                        <td>'.$mtee_cnt[0].'</td>
                         <td><form action="">
                                 <input type="submit" value="add mentor"/>
                             </form>
